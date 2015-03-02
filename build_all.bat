42c1e8f54c4d80f78f11c4a720417299cf7be6dc
Gerald Bauer <gerald.bauer@gmail.com>
2015-03-02 15:33:09 +0100

    add batch script

---
+++ build_all.bat
@@ -0,0 +1,16 @@
+
+
+rem ## rake beer:book:dl SETUP=at
+
+rake beer:run SETUP=at
+rake beer:run SETUP=cz
+
+rake world:run SETUP=countries
+
+
+rake ft:run SETUP=worldcup
+
+rem ## rake ft:run SETUP=cl2014-15    -- fix: northern ireland code (nd->nir)
+
+rake ft:run SETUP=en2014-15
+rake ft:run SETUP=en
